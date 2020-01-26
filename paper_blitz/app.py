790263c82318f03ea64a19a72b812aa9a52c1998
Noezor <noe.pion@gmail.com>
2020-01-26 09:52:46 +0100

    added arxiv_parsing

--- paper_blitz/app.py
+++ paper_blitz/app.py
@@ -1,39 +1,34 @@
 #!flask/bin/python
 import sys
-print(sys.path)
+sys.path.append("/home/pionn/paper_blitz")
 
-from articles.article import Article, ArticleSchema
+from paper_blitz.articles.article import Article, ArticleSchema
+from paper_blitz.articles.arxiv_parsing import Arxiv_Parser
 from flask import jsonify, abort, request
 
-from config import app
+from paper_blitz.config import app, db
+
 
 @app.route('/api/v0.1/article/arxiv_link', methods=['POST'])
 def add_article():
     """
     Creates a new article
     """
-    print(request)
-    print(request.data)
-    print(request.json)
-    print(request.form)
     link = request.json.get('link','')
-    print(link)
-    exist_article = Article.query.filter(Article.link == link) \
-        .one_or_none()
+    exist_article = Article.query.filter(Article.link == link).one_or_none()
 
-
+    article_parser = Arxiv_Parser()
     if not exist_article :
         schema = ArticleSchema()
-        new_article = schema.load(article, session = db.session).data
-
-        db.session.add(new_article)
+        article = article_parser.scrape(link)
+        db.session.add(article)
         db.session.commit()
 
-        return schema.dump(new_article).data, 201
+        return schema.dump(article), 201
 
     else :
         abort(409, f"Article {link} already in stack")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
