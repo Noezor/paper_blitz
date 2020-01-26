8c388b2a60270c2a412608aaa53d220fc9ae1b1e
Noezor <noe.pion@gmail.com>
2020-01-26 09:52:32 +0100

    Added arxiv_parsing

--- paper_blitz/articles/arxiv_parsing.py
+++ paper_blitz/articles/arxiv_parsing.py
@@ -1,16 +1,38 @@
-from article import Article
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+
+from paper_blitz.articles.article import Article
 
 class Arxiv_Parser:
-    def parse(self, link : str) -> Article:
-        return Article(link = link, title = title, authors = authors, date_publication = date_publication,
-            venue = venue)
+    def scrape(self, link : str) -> Article:
+        link_url = self.correct_link(link)
 
-    def corrects_link(self, link : str) -> str:
+        response = requests.get(link_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        
+        title = self.get_title(soup)
+        publication_date = self.get_submission_date(soup)
+        authors = self.get_authors(soup)
+
+        return Article(link = link, title = title, authors = authors)
+
+    def get_title(self, soup):
+        return re.sub("\[[0-9]*\.[0-9]*\] ","",soup.title.string)
+
+    def get_submission_date(self, soup):
+        return soup.find("div", class_= "dateline").string.lstrip().lstrip("(Submitted on ").rstrip(')')
+
+    def get_authors(self, soup):
+        return soup.find("div", class_= "authors").get_text().lstrip("Authors:")
+
+    def correct_link(self, link : str) -> str:
         """
         returns corrected arxiv link
         """
-        link = self._correct_pdf_link(self, link)
+        link = self._correct_pdf_link(link)
         return link
 
-    def _correct_pdf(self, link : str ) -> str:
+    def _correct_pdf_link(self, link : str ) -> str:
         return link.replace('/pdf/', '/abs/')
