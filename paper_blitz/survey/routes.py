23f4041748de87e2cf5299b0c404604f6021927a
Noezor <noe.pion@gmail.com>
2020-02-10 23:06:55 +0100

    added routes

---
+++ paper_blitz/survey/routes.py
@@ -0,0 +1,47 @@
+#!flask/bin/python3
+from paper_blitz.articles.article import Article
+from paper_blitz.stack.model import Stack, Participant, Group
+from paper_blitz.survey.model import Vote
+from paper_blitz.survey.create_survey import Article_Selector_Survey
+from flask import jsonify, abort, request
+
+from paper_blitz.config import app, db
+
+
+@app.route('/api/v0.1/vote/submit', methods=['POST'])
+def add_vote():
+    """
+    Creates a new article
+    """
+    article_name = request.json.get('article_name','')
+    username = request.json.get('username','')
+    user = Participant.query.find(Participant.username == username).first_or_404()
+
+    article = Article.query.filter(Article.name == article_name).first_or_404()
+    group = user.group   
+    stack_entry = Stack.query.filter(Stack.article == exist_article & Stack.group == group).first_or_404()
+    
+    vote = Vote(voter = user, article = stack_entry)
+    db.session.add(vote)
+    db.session.commit()
+
+    return f"Vote added for {user} on {article}", 201
+
+@app.route('/api/v0.1/vote/submit', methods=['GET'])
+def get_articles_survey():
+    """
+    Get articles of survey
+    """
+    if 'groupname' in request.args:
+        groupname = request.args['groupname']
+    else:
+        return "Error: no groupname proivded, please provide one."
+
+    group = Group.query.filter(Group.name == groupname).first_or_404()
+
+    article_selector = Article_Selector_Survey()
+    new_stack_entries, old_stack_entries = article_selector.select_articles(group)
+
+    
+
+
