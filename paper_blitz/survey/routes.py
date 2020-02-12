433ec8047f978323c5870b912340cdd6b9834d08
Noezor <noe.pion@gmail.com>
2020-02-12 20:44:05 +0100

    corrected routes

--- paper_blitz/survey/routes.py
+++ paper_blitz/survey/routes.py
@@ -1,6 +1,7 @@
 #!flask/bin/python3
 from paper_blitz.articles.article import Article
 from paper_blitz.stack.model import Stack, Participant, Group
+from paper_blitz.stack.schema import Article_Stack_Schema
 from paper_blitz.survey.model import Vote
 from paper_blitz.survey.create_survey import Article_Selector_Survey
 from flask import jsonify, abort, request
@@ -27,7 +28,7 @@ def add_vote():
 
     return f"Vote added for {user} on {article}", 201
 
-@app.route('/api/v0.1/vote/submit', methods=['GET'])
+@app.route('/api/v0.1/survey/get', methods=['GET'])
 def get_articles_survey():
     """
     Get articles of survey
@@ -35,13 +36,16 @@ def get_articles_survey():
     if 'groupname' in request.args:
         groupname = request.args['groupname']
     else:
-        return "Error: no groupname proivded, please provide one."
-
+        return "Error: no groupname provided, please provide one."
+    print("OK")
     group = Group.query.filter(Group.name == groupname).first_or_404()
 
     article_selector = Article_Selector_Survey()
     new_stack_entries, old_stack_entries = article_selector.select_articles(group)
-
+    
+    schema = Article_Stack_Schema(many = True)
+    dump = {"old" : schema.dump(new_stack_entries), "new" : schema.dump(old_stack_entries)}
+    return jsonify(dump), 201
     
 
 
